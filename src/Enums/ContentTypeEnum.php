36f9c7da8cb2e99be5d58fe7f26c0cf6770a99f0
Bogdan <80489373+aw3r1se@users.noreply.github.com>
2023-04-21 22:32:15 +0500

    Update ContentTypeEnum.php

--- src/Enums/ContentTypeEnum.php
+++ src/Enums/ContentTypeEnum.php
@@ -5,11 +5,11 @@ namespace Aw3r1se\AdvancedNotifications\Enums;
 use Aw3r1se\AdvancedNotifications\Enums\Contracts\ContentTypeEnumInterface;
 use Aw3r1se\AdvancedNotifications\Enums\Traits\HasNameTrait;
 
-enum ContentTypeEnum implements ContentTypeEnumInterface
+enum ContentTypeEnum: string implements ContentTypeEnumInterface
 {
     use HasNameTrait;
 
-    case TITLE;
-    case SUBJECT;
-    case TEXT;
+    case TITLE = 'TITLE';
+    case SUBJECT = 'SUBJECT';
+    case TEXT = 'TEXT';
 }
