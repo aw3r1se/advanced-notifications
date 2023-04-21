3cc9a6c1ac8b5d0298ceec198feb1c77bdd9f47d
Bogdan <80489373+aw3r1se@users.noreply.github.com>
2023-04-21 22:32:27 +0500

    Update LocaleEnum.php

--- src/Enums/LocaleEnum.php
+++ src/Enums/LocaleEnum.php
@@ -6,12 +6,12 @@ use Aw3r1se\AdvancedNotifications\Enums\Contracts\LocaleEnumInterface;
 use Aw3r1se\AdvancedNotifications\Enums\Traits\HasMatchTrait;
 use Aw3r1se\AdvancedNotifications\Enums\Traits\HasNameTrait;
 
-enum LocaleEnum implements LocaleEnumInterface
+enum LocaleEnum: string implements LocaleEnumInterface
 {
     use HasNameTrait;
     use HasMatchTrait;
 
-    case EN;
-    case RU;
-    case KZ;
+    case EN = 'EN';
+    case RU = 'RU';
+    case KZ = 'KZ';
 }
